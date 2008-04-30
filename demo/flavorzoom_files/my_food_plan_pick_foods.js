cf8d90c2f0e77da6835b88ae46be3ff3b83abe45
Greg Weber <greg@grubtop.(none)>
2008-04-30 00:02:26 -0500

    Demo files added

---
+++ demo/flavorzoom_files/my_food_plan_pick_foods.js
@@ -0,0 +1,16 @@
+$(function() { 
+  var theTable = $('table.food_planner')
+
+  theTable.find("tbody > tr").find("td:eq(1)").mousedown(function(){
+    $(this).prev().find(":checkbox").click()
+  });
+
+  $("#filter").keyup(function() {
+    $.uiTableFilter( theTable, this.value );
+  })
+
+  $('#filter-form').submit(function(){
+    theTable.find("tbody > tr:visible > td:eq(1)").mousedown();
+    return false;
+  }).focus(); //Give focus to input field
+});  
