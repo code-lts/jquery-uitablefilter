b81b00288c8f87a0dcd99186ab622d922e82d21f
William Desportes <williamdes@wdes.fr>
2020-04-18 14:13:43 +0200

    Format example JS file

--- demo/flavorzoom_files/my_food_plan_pick_foods.js
+++ demo/flavorzoom_files/my_food_plan_pick_foods.js
@@ -1,16 +1,16 @@
-$(function() { 
+$(function () {
   var theTable = $('table.food_planner')
 
-  theTable.find("tbody > tr").find("td:eq(1)").mousedown(function(){
+  theTable.find("tbody > tr").find("td:eq(1)").mousedown(function () {
     $(this).prev().find(":checkbox").click()
   });
 
-  $("#filter").keyup(function() {
-    $.uiTableFilter( theTable, this.value );
+  $("#filter").keyup(function () {
+    $.uiTableFilter(theTable, this.value);
   })
 
-  $('#filter-form').submit(function(){
+  $('#filter-form').submit(function () {
     theTable.find("tbody > tr:visible > td:eq(1)").mousedown();
     return false;
   }).focus(); //Give focus to input field
-});  
+});
